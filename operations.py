b747dd78b4aaa7cf81caf606d419ab666f11e85f
NicholasPaquin <43181942+NicholasPaquin@users.noreply.github.com>
2019-07-16 15:20:54 -0400

    started adding activation functions

--- operations.py
+++ operations.py
@@ -7,3 +7,22 @@ def add(vals):
     for i in vals:
         sum += i
     return sum
+
+
+class ActivationFunctions:
+    def relu(self, weight):
+        return weight if weight > 0 else 0
+
+    def sigmoid(self, weight):
+        pass
+
+    def l_relu(self, weight, slope=0.1):
+        return weight if weight > 0 else weight*slope
+
+    def tanh(self, weight):
+        pass
+
+    def softmax(self, weight):
+        pass
+
+
